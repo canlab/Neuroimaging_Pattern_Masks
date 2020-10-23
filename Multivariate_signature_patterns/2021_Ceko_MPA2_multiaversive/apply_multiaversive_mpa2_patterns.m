8d14d72dacc6e1e2d61405184b5056c78c286a59
Yoni Ashar <yoniashar@gmail.com>
2020-10-23 13:53:05 -0400

    apply mpa2

---
+++ Multivariate_signature_patterns/2021_Ceko_MPA2_multiaversive/apply_multiaversive_mpa2_patterns.m
@@ -0,0 +1,15 @@
+% Given an fmri_data object, will return pattern expressions for the 5
+% patterns in MPA2 multiaversive
+
+function pexp_tabl = apply_multiaversive_mpa2_patterns(dat)
+
+    pats = load_image_set('mpa2');
+    
+    pexps = apply_mask(dat, pats, 'pattern_expression');
+    
+    pexp_tabl = array2table(pexps, 'VariableNames', {'general' 'mechanical' 'sound' 'thermal' 'visual'});
+    
+end
+    
+    
+
