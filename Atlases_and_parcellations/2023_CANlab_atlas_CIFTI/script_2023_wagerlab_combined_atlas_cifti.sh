fd1193d77ffdcfe00eb9012d5f61c9e352946380
Bogdan Petre <f0042vm@discovery7.hpcc.dartmouth.edu>
2023-10-05 18:34:37 -0400

    added support for use as qsiprep custom atlas

--- Atlases_and_parcellations/2023_CANlab_atlas_CIFTI/script_2023_wagerlab_combined_atlas_cifti.sh
+++ Atlases_and_parcellations/2023_CANlab_atlas_CIFTI/script_2023_wagerlab_combined_atlas_cifti.sh
@@ -5,3 +5,41 @@ wb_command -cifti-create-label canlab_2023_2mm.dlabel.nii \
     -volume subctx_atlas.label.nii hcp_cifti_subctx_labels.nii \
     -left-label /dartfs-hpc/rc/home/m/f0042vm/software/diedrichsen_fs_LR_32/Glasser_2016.32k.L.label.gii \
     -right-label /dartfs-hpc/rc/home/m/f0042vm/software/diedrichsen_fs_LR_32/Glasser_2016.32k.R.label.gii
+
+
+# create qsiprep atlas config file
+cat <<END > atlas_config.json
+{
+  "canlab2023": {
+    "file": "canlab_2023_2mm.nii.gz",
+    "node_names": [
+END
+
+wb_command -cifti-label-export-table canlab_2023_2mm.dlabel.nii 1 canlab_2023_2mm_labels.txt
+
+# the way we handle "," and newlines below is convoluted. It's intention is to
+# have a "," after every newline except for the last in a list
+n_ROI=$(echo $(cat canlab_2023_2mm_labels.txt | wc -l)/2 | bc)
+for ROI in $(cat canlab_2023_2mm_labels.txt | grep -v ^[0-9]); do 
+    echo ","
+    echo -n "      \"$ROI\""
+done | tail -n $n_ROI >> atlas_config.json
+
+cat <<END >> atlas_config.json
+
+    ],
+    "node_ids": [
+END
+
+for i in $(seq 1 $n_ROI); do 
+    echo ","
+    echo -n "      $i"
+done | tail -n $n_ROI >> atlas_config.json
+    
+cat <<END >> atlas_config.json
+
+    ]
+  }
+}
+END
+
