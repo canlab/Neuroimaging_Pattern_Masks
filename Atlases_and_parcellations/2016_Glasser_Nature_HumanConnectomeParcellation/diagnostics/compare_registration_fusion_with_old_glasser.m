99e0ceb35d5e4667b85ffef434b07d41b0cc6fbf
Bogdan Petre <bogpetre@gmail.com>
2023-11-06 16:11:04 -0500

    Updated dice coefficients for bmrk5 N=88 models and added new evaluations of ROI averages

--- Atlases_and_parcellations/2016_Glasser_Nature_HumanConnectomeParcellation/diagnostics/compare_registration_fusion_with_old_glasser.m
+++ Atlases_and_parcellations/2016_Glasser_Nature_HumanConnectomeParcellation/diagnostics/compare_registration_fusion_with_old_glasser.m
@@ -21,8 +21,13 @@ end
 
 figure(1)
 hist(dicecoef);
-title('NN Glasser vs. RF-ANTs Glasser')
+title({'Old (Horn 2016) Glasser vs.','New Registration Fusion Glasser'})
 xlabel('Dice Coefficient ')
+ylabel('Number of Parcels')
+xlim([0.2,1])
+set(gca,'FontSize',13)
+pos = get(gcf,'Position');
+set(gcf,'Position',[pos(1:2),388,330])
 saveas(gcf,'dice_coef_old_vs_new_glasser.png')
 
 old = old.remove_empty;
@@ -57,8 +62,12 @@ end
 
 figure(11);
 hist(dicecoef);
-title('NN Glasser vs. RF-ANTs Glasser, after masking latter by former')
+title({'Old (Horn 2016) Glasser vs.','New registration fusion Glasser','after masking latter by former'})
 xlabel('Dice Coefficient');
+set(gca,'FontSize',13)
+pos = get(gcf,'Position');
+set(gcf,'Position',[pos(1:2),388,330])
+ylabel('Number of parcels')
 saveas(gcf,'dice_hist_old_vs_new_glasser_masked_by_old.png')
 
 old = old.remove_empty;
@@ -80,6 +89,8 @@ fmriprep20 = load_atlas('glasser_fmriprep20');
 fsl6 = fsl6.resample_space(fmriprep20,'nearest');
 fsl6 = fsl6.threshold(0.2);
 fmriprep20 = fmriprep20.threshold(0.2);
+fsl6.probability_maps = [];
+fmriprep20.probability_maps = [];
 
 assert(all(ismember(fsl6.labels, fmriprep20.labels)))
 labels = fsl6.labels;
@@ -93,8 +104,13 @@ end
 
 figure(21);
 hist(dicecoef);
-title('RF-ANTs Glasser (fmriprep) vs. RF-ANTs Glasser (fsl)')
+title({'MNI152NLin2009cAsym (fmriprep) vs.','MNI152NLin6Asym (fsl)'})
 xlabel('Dice Coefficient');
+ylabel('Number of parcels');
+xlim([0.2,1])
+set(gca,'FontSize',13)
+pos = get(gcf,'Position');
+set(gcf,'Position',[pos(1:2),388,330])
 saveas(gcf,'dice_hist_fmriprep_vs_fsl_glassers.png')
 
 fmriprep20 = fmriprep20.remove_empty;
@@ -119,29 +135,16 @@ lh_lbls = readtable('../lctx_labels.txt');
 rh_lbls = readtable('../rctx_labels.txt');
 lbls = [lh_lbls{:,1}; rh_lbls{:,1}];
 
-pmap = {};
-for study = {'bmrk5', 'paingen'}
-    pdata = fmri_data(sprintf('../%s_%s.nii.gz', study{1}, SPACE));
-    
-    pmap{end+1} = zeros(size(pdata.dat,1), length(lbls));
-    for i = 1:length(lbls)
-        pmap{end}(:,i) = mean(pdata.dat == i,2);
-    end
-end
-
-pdata = pdata.get_wh_image(1);
-
-glasser = cell(length(pmap),1);
-for i = 1:length(pmap)
-    pdata.dat = pmap{i};
-    glasser{i} = atlas(pdata);
+glasser = cell(2,1);
+studies = {'paingen','bmrk5'};
+for i = 1:length(glasser)
+    study = studies{i}
+    glasser{i} = atlas(sprintf('../%s_%s_atlas.nii.gz', study, SPACE));
     glasser{i}.labels = lbls';
-    glasser{i}.probability_maps = sparse(glasser{i}.probability_maps);
 end
 
 % save individual atlases for inspection
-glasser{1} = glasser{1}.threshold(0.2);
-glasser{2} = glasser{2}.resample_space(glasser{1}, 'nearest').threshold(0.2);
+glasser{2} = glasser{2}.resample_space(glasser{1}, 'nearest');
 
 
 assert(all(ismember(glasser{1}.labels, glasser{2}.labels)))
@@ -156,8 +159,13 @@ end
 
 figure(21);
 hist(dicecoef);
-title('RF-ANTs Glasser (paingen) vs. RF-ANTs Glasser (bmrk5), MNI152NLin2009cAsym space')
+title({'Paingen vs.','Bmrk5 parcels'});
 xlabel('Dice Coefficient');
+ylabel('Number of parcels')
+xlim([0.2,1])
+set(gca,'FontSize',13)
+pos = get(gcf,'Position');
+set(gcf,'Position',[pos(1:2),388,330])
 saveas(gcf,'dice_hist_paingen_vs_bmrk5_glassers_MNI152NLin2009cAsym.png')
 
 fmriprep20 = fmriprep20.remove_empty;
