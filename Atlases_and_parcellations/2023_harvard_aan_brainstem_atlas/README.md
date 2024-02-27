b3c8ca21ee8942e2e1d47671e120c527619a8b6f
Bogdan Petre <bogpetre@gmail.com>
2024-02-27 16:25:02 -0500

    initial upload

---
+++ Atlases_and_parcellations/2023_harvard_aan_brainstem_atlas/README.md
@@ -0,0 +1,47 @@
+## Overview
+
+This atlas is based on histology, immunochemistry and ex vivo DWI tractography 
+in 3 postmortem individuals. The resulting parcels have been extensively 
+characterized in terms of their cortical, subcoritcal and commisural 
+structural connectivity, and functional connectivity with resting state 
+functional connectivity, specifically the DMN (Edlow, 2023).
+
+Although the measurement techniques are extensively documented (Edlow 2012, 
+2023), technical details of the projection to MNI space are obscure. The atlas
+is probablistic but it's not clear what the probabilities represent given that
+3 participants is insufficient to meaningfully calibrate label assignment 
+probabilities. The atlas is in MNI 1mm space but it's not clear what template
+was used to achieve this projection. Based on the publication date of the 
+original atlas (v1.0), the use of FSL tools for tractography, and under the
+assumption that the same templates were used for the current atlas version 
+(v2.0, a safe assumption: differences with version 1.0 are detailed here:
+https://datadryad.org/stash/dataset/doi:10.5061/dryad.zw3r228d2 and they are
+minor as far as spatial characteristics are concerned), I've assumed they 
+used the FSL standard 1mm template, which is MNI152NLin6Asym, and used the
+subcortically weighted alignment from MNI152NLin6Asym to MNI152NLin2009cAsym
+space in the templates/transforms folder to project it to MNI152NLin2009cAsym
+space. That said, given the obscurities associated with the probability values 
+and template selection the boundaries of these regions should be considered 
+approximate.
+
+Consider comparing regions with the Bianciardi brainstem nuclei. All of these
+are also available there, albeit under a more restrictive distribution license.
+
+## License
+
+This dataset is licensed under creative commons (CC0) 1.0 Universal and is in
+the public domain.
+
+## External Links
+
+* https://datadryad.org/stash/dataset/doi:10.5061/dryad.zw3r228d2
+* https://www.nmr.mgh.harvard.edu/resources/aan-atlas
+
+
+## References
+
+Note: Edlow 2023a is the recommended citation
+
+* Edlow, BL; Kinney, HC (2023a), Harvard Ascending Arousal Network Atlas – Version 2.0, Dryad Digital Repository, https://doi.org/10.5061/dryad.zw3r228d2
+* Brian L Edlow, Mark Olchanyi, Holly J Freeman, Jian Li, Chiara Maffei, Samuel B Snider, Lilla Zollei, Juan Eugenio Iglesias, Jean Augustinack, Yelena G. Bodien, Robin L Haynes, Douglas N Greve, Bram R Diamond, Allison Stevens, Joseph T Giacino, Christophe Destrieux, Andre van der Kouwe, Emery N Brown, Rebecca D Folkerth, Bruce Fischl, Hannah C Kinney. (2023b), Sustaining wakefulness: Brainstem connectivity in human consciousness bioRxiv 2023.07.13.548265; doi: https://doi.org/10.1101/2023.07.13.548265
+* Edlow BL, Takahashi E, Wu O, Benner T, Dai G, Bu L, Grant PE, Greer DM, Greenberg SM, Kinney HC, Folkerth RD. Neuroanatomic connectivity of the human ascending arousal system critical to consciousness and its disorders. (2012) Journal of Neuropathology and Experimental Neurology. 71:531-546.  PMCID: PMC3387430.
---
+++ Atlases_and_parcellations/2023_harvard_aan_brainstem_atlas/aan_MNI152NLin6Asym_create_atlas.m
@@ -0,0 +1,166 @@
+addpath('/home/bogdan/.matlab/spm/spm12');
+addpath(genpath('/home/bogdan/.matlab/canlab/CanlabCore'))
+addpath(genpath('/home/bogdan/.matlab/canlab/Neuroimaging_Pattern_Masks'))
+
+space_description = 'MNI152NLin6Asym';
+atlas_name = sprintf('harvard_aan_v2_%s', space_description);
+references = char({'Edlow, BL; Kinney, HC (2023a), Harvard Ascending Arousal Network Atlas – Version 2.0, Dryad Digital Repository, https://doi.org/10.5061/dryad.zw3r228d2'});
+
+dosave = true;
+
+% there are two available parcellations. One based on T1 and T2 contrast,
+% the other using FA and T1 contrast. The former has more regions, but is
+% not as well segmented. I particular it overlaps with the coricospinal
+% tracts.
+parcellation_files = arrayfun(@(x1)[x1.folder, '/', x1.name], dir('src/*nii.gz'), 'UniformOutput', false);
+
+
+tbl = readtable(which('harvard_aan_labels.csv'));
+labels = cellfun(@(x1)strrep(x1,'-','_'),tbl.labels,'UniformOutput',false);
+labels = labels(:)';
+label_descriptions = tbl.label_descriptions(:)';
+
+parcellation = fmri_data(parcellation_files);
+
+n_regions = length(parcellation_files);
+lateralized_maps = find(contains(parcellation_files, {'_L_','_R_'}));
+nonlateralized_maps = find(contains(parcellation_files, {'DR','MnR','PAG','VTA'}));
+
+pmap = zeros(size(parcellation.dat,1), n_regions);
+new_labels = {};
+new_descriptions = {};
+for i = 1:length(lateralized_maps)
+    map_ind = lateralized_maps(i);
+    region = regexprep(parcellation_files{map_ind},'.*AAN_([A-Za-z]*)_([LR])_MNI.*','$1');
+    h = regexprep(parcellation_files{map_ind},'.*AAN_[A-Za-z]*_([LR])_MNI.*','$1');
+    this_label = [h, '_', region];
+    switch h
+        case 'L'
+            suffix = '(left)';
+        case 'R'
+            suffix = '(right)';
+    end
+    this_description = [label_descriptions{contains(labels, region)}, ' ', suffix];
+
+    pmap(:,i) = parcellation.dat(:,map_ind);
+    new_labels{end+1} = this_label;
+    new_descriptions{end+1} = this_description;
+end
+
+for i = 1:length(nonlateralized_maps)
+    map_ind = nonlateralized_maps(i);
+    this_label = regexprep(parcellation_files{map_ind},'.*AAN_([A-Za-z]*)_.*','$1');
+    
+    this_description = [label_descriptions{contains(labels, this_label)}, ' ', suffix];
+
+    pmap(:,i+length(lateralized_maps)) = parcellation.dat(:,map_ind);
+    new_labels{end+1} = this_label;
+    new_descriptions{end+1} = this_description;
+end
+parcellation.dat = pmap;
+
+% there's a bug with the PTg_L file, and it's actually bilateral, so let's
+% subtract it out
+parcellation.dat(:,contains(new_labels,'L_PTg')) = parcellation.dat(:,contains(new_labels,'L_PTg')) - parcellation.dat(:,contains(new_labels,'R_PTg'));
+
+atlas_obj = atlas(parcellation, ...
+    'labels',new_labels, ...
+    'label_descriptions', new_descriptions, ...
+    'atlas_name', atlas_name ,...
+    'space_description', space_description, ...
+    'references', references);
+
+
+% Process object
+% -----------------------------------------------------------------------
+
+
+% Check display
+% -----------------------------------------------------------------------
+
+% Display with unique colors for each region:
+atlas_obj.orthviews('unique',which([space_description '_T1_1mm.nii.gz']));
+
+
+% Convert to regions
+% -----------------------------------------------------------------------
+
+r = atlas2region(atlas_obj);
+
+% Display on montage (colors may not be the same!):
+% montage(r);
+ 
+ %% save figure
+
+cmap = scn_standard_colors(n_regions/2);
+cmap = cell2mat(cat(2,cmap'));
+cmap = [cmap; cmap];
+
+if dosave
+   
+    o2 = canlab_results_fmridisplay([], 'full hcp', ...
+        'overlay', which(sprintf('%s_T1_1mm.nii.gz',space_description)));
+    brighten(.6)
+    
+    o2 = montage(r, o2, 'wh_montages', 1:2, 'indexmap', cmap);
+    
+    savedir = fullfile(pwd, 'png_images');
+    if ~exist(savedir, 'dir'), mkdir(savedir); end
+    
+    scn_export_papersetup(600);
+    savename = fullfile(savedir, sprintf('%s_montage.png', atlas_name));
+    saveas(gcf, savename);
+
+    
+end
+
+%% save object
+
+if dosave
+    
+    savename = sprintf('%s_atlas_object.mat', atlas_name);
+    save(savename, 'atlas_obj');
+    
+end
+
+%% write - this writes only the label image
+
+if dosave
+    
+    savename = sprintf('%s_atlas_regions.img', atlas_name);
+    atlas_obj.fullpath = fullfile(pwd, savename);
+    write(atlas_obj,'overwrite');
+    
+end
+
+%% Turn regions into separate list of names, for canlab_load_ROI
+% which loads regions by name from mat files.
+
+clear region_names
+labels = atlas_obj.labels;
+for i = 1:length(r)
+    
+    eval([labels{i} ' = r(i);']);
+    
+    region_names{i} = r(i).shorttitle;
+    
+end
+
+savename = sprintf('%s_atlas_regions.mat', atlas_name);
+save(savename, 'r', 'region_names', labels{:});
+
+%%
+if dosave
+    
+    figure; han = isosurface(atlas_obj);
+    
+    arrayfun(@(x1)(set(x1,'FaceAlpha', .5)), han)
+    view(135, 20)
+    lightFollowView;
+    lightRestoreSingle
+    axis off
+    
+    savename = fullfile(savedir, sprintf('%s_isosurface.png', atlas_name));
+    saveas(gcf, savename);
+    
+end
